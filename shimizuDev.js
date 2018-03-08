ff084cdb45ab02be9801e08428b5328e30605279
xshimizu-koujirou <xshimizu.koujirou@contract.isid.co.jp>
2018-03-08 10:11:18 +0900

    no message

---
+++ shimizuDev.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const https = require('https');
+
+let send = (data, callback) => {
+	let body = JSON.stringify(data);
+
+	let req = https.request({
+		hostname: "api.line.me",
+		port: 443,
+		path: "/v2/bot/message/reply",
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Content-Length": Buffer.byteLength(body),
+			"Authorization": "Bearer " + process.env.CHANNEL_ACCESS_TOKEN
+		}
+	});
+
+	req.end(body, (err) => {
+		err && console.log(err);
+		callback(err);
+	});
+}
+
+exports.handler = (event, context, callback) => {
+	let result = event.events && event.events[0];
+	if (result.message.type == "text") {
+		
+		console.log(result);
+		if (result.message.text == "アースリンク") {
+			let content = event.events[0] || {};
+			let message = {
+				"replyToken":result.replyToken,
+				"messages": [{
+					"type": "template",
+					"altText": "this is a image carousel template",
+					"template": {
+						"type": "image_carousel",
+						"columns": [{
+							"imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRoOYethc6eFEJGPPzd2iYSiJZOZxGW17vl9BpUPmL6MeoRVJR",
+							"action": {
+								"type": "uri",
+								"uri": "http://www.earthlink.co.jp/"
+							}
+						}]
+					}
+				}]
+			};
+			send(message, () => {
+				callback();
+			});
+		} else {
+			let content = event.events[0] || {};
+			let message = {
+				"replyToken":result.replyToken,
+				"messages": [
+					{
+						"type": "text",
+						"text": content.message.text
+					}
+				]
+			};
+			send(message, () => {
+				callback();
+			});
+		}
+	} else if (result.message.type == "sticker") {
+		let content = event.events[0] || {};
+		let message = {
+			"replyToken":result.replyToken,
+			"messages": [
+				{
+					"type": "sticker",
+					"packageId":content.message.packageId,
+					"stickerId":content.message.stickerId
+				}
+			]
+		};
+		send(message, () => {
+			callback();
+		});
+
+	} else {
+		callback();
+	}
+};
+
